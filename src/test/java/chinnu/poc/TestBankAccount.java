6a1bda9e398ad077ddaab5f1b58c4e4f8746c71e
Chinnu <40233756+jeenuchinnu@users.noreply.github.com>
2019-05-25 12:42:03 +0530

    Update TestBankAccount.java
    test added

--- src/test/java/chinnu/poc/TestBankAccount.java
+++ src/test/java/chinnu/poc/TestBankAccount.java
@@ -6,4 +6,13 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
+  @Test
+  public void testDebitWithSuffBal{
+  
+    BankAccount acc = new BankAccount(10);
+    double amt = acc.debit(5);
+    Assert.assertEquals(5.0,amt);
+  
+  }
+  
 }
