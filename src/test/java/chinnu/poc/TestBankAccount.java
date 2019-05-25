3770694aeaaf1dd610ae6fbdb6044e177ec2e884
Chinnu <40233756+jeenuchinnu@users.noreply.github.com>
2019-05-25 12:45:18 +0530

    Update TestBankAccount.java

--- src/test/java/chinnu/poc/TestBankAccount.java
+++ src/test/java/chinnu/poc/TestBankAccount.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestBankAccount {
 
   @Test
-  public void testDebitWithSuffBal{
+  public void testDebitWithSuffBal(){
   
     BankAccount acc = new BankAccount(10);
     double amt = acc.debit(5);
